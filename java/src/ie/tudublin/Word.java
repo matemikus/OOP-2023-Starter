cd870ae7488352cd9c61f2c0ca11fcf429a08121
matemikus <c21379483@mytudublin.ie>
2023-04-26 16:55:44 +0100

    made word class

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -1,5 +1,36 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 public class Word {
-    
+    private String word;
+    ArrayList<Follow> follows = new ArrayList<>();
+
+    public Word(String word) {
+        this.word = word;
+        this.follows = new ArrayList<>();
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public ArrayList<Follow> getFollows() {
+        return this.follows;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setFollows(ArrayList<Follow> follows) {
+        this.follows = follows;
+    }
+
+    public void addFollow(Follow follow) {
+        this.follows.add(follow);
+    }
+
+   
 }
+
